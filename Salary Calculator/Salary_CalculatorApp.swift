3d90192f73623248db1c11acd3c2867faf64d59d
Wenqi Zheng <wenqizheng326@gmail.com>
2024-07-05 03:15:21 -0400

    Initial Commit

---
+++ Salary Calculator/Salary_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Salary_CalculatorApp.swift
+//  Salary Calculator
+//
+//  Created by Wenqi Zheng on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Salary_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
